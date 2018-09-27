62c46b1509fad1a21d16be425827d94ddc603e6c
Mason Nesbitt <masonnesbitt@macbook-pro-10.local>
2018-09-27 08:35:04 -0500

    Initial Commit

---
+++ Portfolio App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Portfolio App
+//
+//  Created by Mason Nesbitt on 9/27/18.
+//  Copyright © 2018 Mason Nesbitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
